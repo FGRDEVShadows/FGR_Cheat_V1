ec9f08d06d28e6456df9af702fb082158a4be00a
FGRDEVShadows <fgrdevshadows@gmail.com>
2024-07-22 01:37:20 +0300

    Update Cheat.lua

--- Cheat.lua
+++ Cheat.lua
@@ -2,4 +2,4 @@ wait(1)
 
 -- Cheat
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FGRDEVShadows/FGR_Bot_V1/main/bot.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FGRDEVShadows/FGR_Cheat_V1/main/Cheatr.lua"))()
